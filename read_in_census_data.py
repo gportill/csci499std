19946c5584a6256d178d8be79047d36cc6ad4195
Gwenyth Portillo-Wightman <gportill@usc.edu>
2019-03-29 18:40:40 -0700

    examine difference between feature labels for diferent years

--- read_in_census_data.py
+++ read_in_census_data.py
@@ -11,7 +11,7 @@ year = 2005
 for file in census_files:
     year += 1
     # print(str(year))
-    key = str(year) + "_census"
+    key = str(year)
     census_dfs[key] = pd.read_csv(file, encoding='latin-1', index_col=False)
 
 print(census_dfs)
@@ -19,4 +19,9 @@ print(census_dfs)
 # for df in census_dfs:
 #     print(census_dfs[df].head())
 
-print(census_dfs["2006_census"].columns.values)
+print(census_dfs["2006"].columns.values)
+print(census_dfs["2008"].columns.values)
+
+# only difference between 2006 and 2008 is that 2008 has these additional:
+# 'SE_A20001_001' 'SE_A20001_002' 'SE_A20001_003' 'SE_A20001_004' 'SE_A20001_005']
+# health insurance?
