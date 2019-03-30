c4a90fb2b6b6b9ca1977fbce188f5a6853ff1e06
Gwenyth Portillo-Wightman <gportill@usc.edu>
2019-03-30 14:12:08 -0700

    fix std data files and read in std data files

--- read_in_std_data.py
+++ read_in_std_data.py
@@ -1,6 +1,9 @@
 import pandas as pd
 
-std_files = ["./all_std_data/2006_std.csv", "./all_std_data/2007_std.csv", "./all_std_data/2008_std.csv", "./all_std_data/2009_std.csv", "./all_std_data/2010_std.csv", "./all_std_data/2011_std.csv", "./all_std_data/2012_std.csv", "./all_std_data/2013_std.csv", "./all_std_data/2014_std.csv", "./all_std_data/2015_std.csv", "./all_std_data/2016_std.csv"]
+std_files = ["./chlam_data/2006_chlamydia.csv", "./chlam_data/2007_chlamydia.csv", "./chlam_data/2008_chlamydia.csv",
+             "./chlam_data/2009_chlamydia.csv", "./chlam_data/2010_chlamydia.csv", "./chlam_data/2011_chlamydia.csv",
+             "./chlam_data/2012_chlamydia.csv", "./chlam_data/2013_chlamydia.csv", "./chlam_data/2014_chlamydia.csv",
+             "./chlam_data/2015_chlamydia.csv", "./chlam_data/2016_chlamydia.csv"]
 
 std_dfs = {}
 
@@ -8,19 +11,5 @@ year = 2005
 
 for file in std_files:
     year += 1
-    print(str(year))
     key = str(year)
     std_dfs[key] = pd.read_csv(file, encoding='latin-1')
-    # should index_col be true or false?
-
-print(std_dfs)
-
-# for df in census_dfs:
-#     print(census_dfs[df].head())
-
-# print(census_dfs["2006"].columns.values)
-# print(census_dfs["2008"].columns.values)
-
-# only difference between 2006 and 2008 is that 2008 has these additional:
-# 'SE_A20001_001' 'SE_A20001_002' 'SE_A20001_003' 'SE_A20001_004' 'SE_A20001_005']
-# health insurance?
