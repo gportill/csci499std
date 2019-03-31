a5d130a6fc66f37304387242793bfcff9a34d2e9
Gwenyth Portillo-Wightman <gportill@usc.edu>
2019-03-30 18:53:31 -0700

    migration data for a single year

--- read_migration_data.py
+++ read_migration_data.py
@@ -44,4 +44,67 @@ for i in range(1, len(county_adj_df)):
 
 print(fips_adj_dict)
 
-# if the "county name" index 5 contains "total migration" or "other" then exclude that row
+# if the "county name" index 5 contains "total migration" or "other" or "tot mig" then exclude that row
+
+# list of state abbreviations
+state_abbrev = ["AR", "AL", "AR", "AZ", "CA", "CO", "CT", "DC", "DE", "FL", "GA",
+        "HI", "IA", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "MA", "MD", "ME",
+        "MI", "MN", "MO", "MS", "MT", "NE", "NV", "NH", "NJ",
+        "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC",
+        "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
+
+year_codes = ["0506", "0607", "0708", "0809", "0910", "1011", "1112", "1213", "1314", "1415", "1516"]
+
+migration_dfs = {}
+
+# for i in range(0,7): # 0506 to 1011
+#     for state in state_abbrev:
+#         file = "./migration_data/" + year_codes[i] + "migrationdata/co" + year_codes[i] + state + "i.xls"
+#         # read csv for this file
+#         # drop rows (as on stack overflow) for the specific words
+#         # then assemble the fips from first four columns
+#         # create data frame with the 2 fips and the numbers from pandas
+#         raw_df = pd.read_excel(file, encoding = "ISO-8859-1")
+#         print(raw_df)
+
+
+i = 0
+state = "AK"
+file = "./migration_data/" + year_codes[i] + "migrationdata/co" + year_codes[i] + state + "i.xls"
+# read csv for this file
+# drop rows (as on stack overflow) for the specific words
+# then assemble the fips from first four columns
+# create data frame with the 2 fips and the numbers from pandas
+col_names = ["state_fips1", "county_fips1", "state_fips2", "county_fips2", "state", "description", "num_returns",
+             "num_exemptions", "agg_adj_gross_income"]
+
+raw_df = pd.read_excel(file, encoding = "ISO-8859-1", skiprows=8, header=None, names=col_names, dtype=str)
+print(len(raw_df.index))
+raw_df = raw_df[~raw_df.description.str.contains("Total")]
+raw_df = raw_df[~raw_df.description.str.contains("Other")]
+raw_df = raw_df[~raw_df.description.str.contains("Tot")]
+raw_df = raw_df[~raw_df.description.str.contains("Foreign")]
+raw_df = raw_df[~raw_df.description.str.contains("Non-Migrants")]
+raw_df = raw_df[~raw_df.description.str.contains("Non-Migrant")]
+print(len(raw_df.index))
+print(raw_df.head(5))
+
+destination_fips = []
+origin_fips = []
+num_exemptions = []
+
+for i in range(0,len(raw_df)):
+    # print(raw_df.iloc[i]['state_fips1'])
+    dest_fips = str(raw_df.iloc[i]['state_fips1']) + str(raw_df.iloc[i]['county_fips1'])
+    ori_fips = str(raw_df.iloc[i]['state_fips2']) + str(raw_df.iloc[i]['county_fips2'])
+    num_exemps = raw_df.iloc[i]['num_exemptions']
+    # print(dest_fips + " " + ori_fips + " " + num_exemps)
+    destination_fips.append(dest_fips)
+    origin_fips.append(ori_fips)
+    num_exemptions.append(num_exemps)
+
+d = {'destination': destination_fips, 'origin': origin_fips, 'num_exemps': num_exemptions}
+
+df = pd.DataFrame(d)
+
+print(df.head(5))
