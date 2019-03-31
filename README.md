d6a6d6fee8fe851684b4302280860f78c5d42d93
Gwenyth Portillo-Wightman <gportill@usc.edu>
2019-03-31 08:49:45 -0700

    update readme and comments in calculate_eng_features

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # csci499std
+
+* calculate_eng_features.py: File where engineered features will be calculated. Currently working on migration feature => *start here*
+
+* parseData.py: ?, not needed
+
+* read_data.py: Class for reading in census data, std data, migration data (raw numbers), and county/neighbors dictionary.
+
+* read_in_census data.py: Don't edit this one (edit function in read_data.py instead). 
+
+* read_in_std_data.py: Don't edit this one (edit function in read_data.py instead). 
+
+* read_migration_data.py: Don't edit this one (edit function in read_data.py instead). 
+
+* std_county_map.py: Constructs maps of STD rates over the years.
--- calculate_eng_features.py
+++ calculate_eng_features.py
@@ -2,16 +2,14 @@ import read_data
 
 # for a particular year
     # for each row of STD data: => county i (for each row, we will look at county i's neighbors in migration data)
-        # store the neighbor's neighbors in a list (take this from
-        # expected_infected = 0
+        # store the county's neighbors in a list (available in adj_fips_dict)
+        # initialize expected_infected = 0
         # for each neighbor j in the list:
             # find neighbor j's migration rate to county i in mig_dfs for this year
             # find neighbor j's total pop in census data for this year
             # find neighbor's std cases in std data for this year
             # calculate std_cases / total_pop * mig_rate
-            # add that calculation to the expected_infected variable
-
-# you HAVE TO CHANGE THE NEIGHBORS LIST / ADJACENCY FUNCTION TO FIPS
+            # add that calculation to the expected_infected variable (so you are adding up expected_infected for all of their neighbors). this number will be the feature in the end.
 
 rd = read_data.ReadData()
 adj_fips_dict = rd.read_county_neighbors()
@@ -23,7 +21,8 @@ years = []
 for i in range(2006, 2017):
     years.append(str(i))
 
-# ---------
+# --------- below this line, working on calculating the migration feature for ONE year (2006) so that
+    # it can later be turned into for loop over all years
 
 i = 0
 year = years[i]
