c50dadb5be01c7934248951ac916ceaefed3613b
Gwenyth Portillo-Wightman <gportill@usc.edu>
2019-04-08 12:49:45 -0700

    add lin regression and log regression

--- logistic_regression.py
+++ logistic_regression.py
@@ -3,44 +3,17 @@ import numpy as np
 import scipy.stats as stats
 import matplotlib.pyplot as plt
 import sklearn.model_selection
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LogisticRegression
 import sklearn.metrics
 
 df = pd.read_excel('./full_features_no_mig_NO_NAN1.xlsx')
 df = df.drop('year', axis=1)
 df = df.drop('fips', axis=1)
 x = df.drop('rate', axis=1)
-# lm = LinearRegression()
 
-# fit_intercept and normalize => parameters to the linear regression object
-
-# lm.fit(x, df.rate)
-# print("Estimated intercept coefficient: " + str(lm.intercept_))
-# print("Number of coefficients: " + str(len(lm.coef_)))
-# coefs_df = pd.DataFrame(zip(x.columns, lm.coef_), columns=['features', 'estimated coefficients'])
-# print(coefs_df)
-
-# should year be included as a feature? **
-# remove fips and years as a feature
-
-# predictions = lm.predict(x)
-# plt.scatter(df.rate, predictions)
-# plt.xlabel("Actual cases")
-# plt.ylabel("Predicted cases")
-# plt.title("Actual cases vs. predicted cases")
-# plt.show()
-
-# mseFull = np.mean((df.rate - lm.predict(x))**2)
-# print(mseFull)
-
-# how to use validation set?
-# add validation set by splitting test
-# x_train_and_val, x_test, y_train_and_val, y_test = sklearn.model_selection.train_test_split(x, df.rate, test_size=0.2, random_state=5)
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, df.rate, test_size=0.2, random_state=5)
 
-# x_train, x_val, y_train, y_val = sklearn.model_selection.train_test_split()
-
-lm = LinearRegression()
+lm = LogisticRegression()
 lm.fit(x_train, y_train)
 pred_train = lm.predict(x_train)
 print("pred_train: " + str(list(pred_train)))
