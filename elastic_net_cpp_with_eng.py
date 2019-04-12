6b47f2b56f5293a2be6b46da7b298dfc6021b5ff
Gwenyth Portillo-Wightman <gportill@usc.edu>
2019-04-11 20:26:53 -0700

    add new models: bagging regression, elastic net, gradient boosting regressor, kernel ridge regression, multilayer perception, random foest. add data frames as excel files

---
+++ elastic_net_cpp_with_eng.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import numpy as np
+import scipy.stats as stats
+import matplotlib.pyplot as plt
+import sklearn.model_selection
+from sklearn.linear_model import ElasticNet
+import sklearn.metrics
+
+
+def create_model(year):
+    filename = "time_dep_t0_" + str(year) + ".xlsx"
+    full_df = pd.read_excel(filename, dtype=float)
+
+    x = full_df.drop('target_t5', axis=1)
+    y = full_df.target_t5.astype(float)
+
+    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.2,
+                                                                                random_state=5)
+    # add regularization to linear model
+    # increase weight of l2 norm or l1 norm
+    # elastic net or kernel ridge regression, decision trees, gradient with shallow trees (max_depth = small)
+    # hamper 0 make them have less complexity.
+    # try less features => bagging regressor to subsample features
+    # try changing target
+    lm = ElasticNet()
+    lm.fit(x_train, y_train)
+    pred_train = lm.predict(x_train)
+    print("training r^2:", sklearn.metrics.r2_score(y_train, pred_train))
+    pred_test = lm.predict(x_test)
+
+    r2 = sklearn.metrics.r2_score(y_test, pred_test)
+    print("r2: " + str(r2))
+
+    plt.scatter(y_test, pred_test)
+    plt.xlabel("Actual cases")
+    plt.ylabel("Predicted cases")
+    plt.title("Actual cases vs. predicted cases in y_test")
+    plt.show()
+
+
+create_model(2010)
---
+++ make_time_dep_dataframe.py
@@ -0,0 +1,103 @@
+import pandas as pd
+
+# makes a data frame for time dependent modes for CASES-PER-PERSON and WITH ENGINEERED FEATURES
+# file name: "time_dep_t0_" + str(year) + ".xlsx"
+# full_df = pd.read_excel(filename, dtype=float)
+
+def build_df(year):
+    if year < 2006 or year > 2011:
+        print("year must be [2006, 2011]")
+        return
+
+    dfs = {}  # access by integer year number, not string
+    num_training_years = 5
+    year_to_predict = year+num_training_years
+    t0 = year  # first year of data
+    j = 0
+
+    cols_to_keep_t0 = ['fips', 'total_pop', 'pop_density', 'male', 'female',
+                       'age_15_to_17', 'age_18_to_24', 'age_25_to_34', 'pop_15_and_older',
+                       'never_married', 'poverty_status_under18_living_in_poverty',
+                       'poverty_status_18_to_64_living_in_poverty',
+                       'poverty_status_65older_living_in_poverty', 'infected_inflow', 'cases_per_person']
+    # 'cases', 'cases_raw'
+
+    cols_to_keep = ['total_pop', 'pop_density', 'male', 'female',
+                       'age_15_to_17', 'age_18_to_24', 'age_25_to_34', 'pop_15_and_older',
+                       'never_married', 'poverty_status_under18_living_in_poverty',
+                       'poverty_status_18_to_64_living_in_poverty',
+                       'poverty_status_65older_living_in_poverty', 'infected_inflow', 'cases_per_person']
+
+    print("t0: " + str(t0))
+    print("year_to_predict: " + str(year_to_predict))
+
+    for i in range(year, year+num_training_years+1):
+        df = pd.read_excel('./full_features_by_year.xlsx', sheet_name=str(i), dtype=str)
+        df = df.drop("Unnamed: 0", axis=1)
+
+        # match all rows of years after t0 match the county rows for t0
+        # discard rows that there is no data for at t0
+        if i > t0:
+            df = df[df['fips'].isin(dfs[t0]['fips'])]
+
+        if i == t0:
+            df = df[cols_to_keep_t0]
+        else:
+            df = df[cols_to_keep]
+
+        # update column names for all dfs
+        updated_col_names = []
+        column_names = df.columns.values
+        for name in column_names:
+            if i == t0 and name == 'year':
+                updated_col_names.append('year')
+                continue
+            if i == t0 and name == 'fips':
+                updated_col_names.append('fips')
+                continue
+            updated_col_names.append(name + "_t" + str(j))
+        df.columns = updated_col_names
+        j += 1
+        dfs[i] = df
+
+    # now remove fips_t0 for dfs[t0]
+    dfs[t0] = dfs[t0].drop('fips', axis=1)
+
+    # concatenate all dfs but the year to predict
+    full_df = dfs[t0].copy()
+    for i in range(t0+1, year_to_predict):
+        full_df = full_df.join(dfs[i])
+
+    # -------- add engineered features -------
+
+    full_df['cases_per_person_t0'] = full_df['cases_per_person_t0'].astype(float)
+    full_df['cases_per_person_t1'] = full_df['cases_per_person_t1'].astype(float)
+    full_df['cases_per_person_t2'] = full_df['cases_per_person_t2'].astype(float)
+    full_df['cases_per_person_t3'] = full_df['cases_per_person_t3'].astype(float)
+    full_df['cases_per_person_t4'] = full_df['cases_per_person_t4'].astype(float)
+
+    # diff_cases_t0_t4
+    full_df['diff_cases_t0_t4'] = full_df['cases_per_person_t4'] - full_df['cases_per_person_t0']
+    # diff_cases_t0_t1
+    full_df['diff_cases_t0_t1'] = full_df['cases_per_person_t1'] - full_df['cases_per_person_t0']
+    # diff_cases_t1_t2
+    full_df['diff_cases_t1_t2'] = full_df['cases_per_person_t2'] - full_df['cases_per_person_t1']
+    # diff_cases_t2_t3
+    full_df['diff_cases_t2_t3'] = full_df['cases_per_person_t3'] - full_df['cases_per_person_t2']
+    # diff_cases_t3_t4
+    full_df['diff_cases_t3_t4'] = full_df['cases_per_person_t4'] - full_df['cases_per_person_t3']
+    # print(full_df.head())
+    # -------- end engineered features --------
+
+    full_df['target_t5'] = dfs[year_to_predict].cases_per_person_t5  # put the variable you want to predict here
+    # now that variable (for y) is called target_t5 in the full_df
+
+    full_df = full_df.dropna()
+
+    file = "time_dep_t0_" + str(year) + ".xlsx"
+
+    full_df.to_excel(file, na_rep="nan", index=False)
+
+
+for i in range(2006, 2012):
+    build_df(i)
